33af54f5f782bd4cac5445bf819c00adf42f0135
bcalderom  <65696302+bcalderom@users.noreply.github.com>
2025-12-30 19:44:48 -0300

    feat: switch to Telescope picker with enhanced file filtering and hidden file support
    - Set lazyvim_picker to "telescope" in config options
    - Removed vim-be-good plugin and dressing.nvim dependency
    - Added comprehensive file_ignore_patterns for .git, .local, go, .thunderbird, cache, .codeium, .windsurf, .obsidian, .npm
    - Configured find_files with rg to include hidden files and follow symlinks
    - Added custom vimgrep_arguments for live_grep with same exclusion patterns
    - Refactored telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,42 +1,118 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-symbols.nvim",
+    },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files Telescope" },
     },
-    opts = {
-      pickers = {
-        find_files = {
-          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-          find_command = { "rg", "--files", "--glob", "!**/.git/*", "-L" },
-        },
-      },
-    },
-  },
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
-  {
-    "nvim-telescope/telescope-symbols.nvim",
+
+    -- NOTE:
+    -- LazyVim (and other specs) may also configure Telescope.
+    -- Using opts=function(_, opts) ensures our settings are MERGED instead of replaced/ignored.
+    opts = function(_, opts)
+      opts.defaults = opts.defaults or {}
+      opts.pickers = opts.pickers or {}
+      opts.pickers.find_files = opts.pickers.find_files or {}
+
+      -- 1) Always hide these paths from results (backend-agnostic; works even if Telescope uses fd)
+      opts.defaults.file_ignore_patterns = vim.list_extend(opts.defaults.file_ignore_patterns or {}, {
+        "/%.git/",
+        "^%.git/",
+        "/%.local/",
+        "^%.local/",
+        "/go/",
+        "^go/",
+        "/%.thunderbird/",
+        "^%.thunderbird/",
+        "/cache/",
+        "^cache/",
+        "/.codeium/",
+        "^.codeium/",
+        "/.windsurf/",
+        "^.windsurf/",
+        "/.obsidian/",
+        "^.obsidian/",
+      })
+
+      -- 2) Exclude at the source for find_files (fast) + include hidden + follow symlinks
+      -- Put this under pickers.find_files so builtins.find_files picks it up reliably.
+      opts.pickers.find_files.find_command = {
+        "rg",
+        "--files",
+        "--hidden",
+        "--follow",
+        "--glob",
+        "!**/.git/*",
+        "--glob",
+        "!**/.local/**",
+        "--glob",
+        "!**/go/**",
+        "--glob",
+        "!**/.thunderbird/**",
+        "--glob",
+        "!**/.cache/**",
+        "--glob",
+        "!**/.windsurf/**",
+        "--glob",
+        "!**/.obsidian/**",
+        "--glob",
+        "!**/.npm/**",
+      }
+
+      -- 3) (Optional but recommended) Make live_grep respect hidden + the same excludes
+      opts.defaults.vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+        "--follow",
+        "--glob",
+        "!**/.git/*",
+        "--glob",
+        "!**/.local/**",
+        "--glob",
+        "!**/go/**",
+        "--glob",
+        "!**/.thunderbird/**",
+        "--glob",
+        "!**/.cache/**",
+        "--glob",
+        "!**/.windsurf/**",
+        "--glob",
+        "!**/.obsidian/**",
+        "--glob",
+        "!**/.npm/**",
+      }
+    end,
+
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      -- Load fzf-native AFTER setup and only once.
+      pcall(telescope.load_extension, "fzf")
+    end,
   },
 
-  -- Custom ripgrep configuration:
+  -- Custom ripgrep configuration notes (kept for reference)
 
   -- I want to search in hidden/dot files.
   -- "--hidden"
   --
   -- I don't want to search in the `.git` directory.
-  -- "--glob")
-  -- "!**/.git/*")
+  -- "--glob"
+  -- "!**/.git/*"
   --
-  --  I want to follow symbolic links
-  -- "-L"
-  --
+  -- I want to follow symbolic links
+  -- "--follow"
 }
