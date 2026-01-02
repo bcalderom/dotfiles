6715f5f2d90fc24773993db3582ff82fe1bfbed8
bcalderom  <65696302+bcalderom@users.noreply.github.com>
2026-01-02 09:16:16 -0300

    feat(nvim): add treesitter parser config and telescope keymaps for notes/config navigation
    - Added nvim-treesitter.lua with auto-install for lua, vim, bash, python, js, html, css, json, yaml, toml, markdown, dockerfile, gitignore parsers
    - Added <leader>en keymap to search Neovim config files
    - Added <leader>fn keymap to search Obsidian engineer vault and Documents/Notes
    - Commented out <leader>fp keymap for plugin file search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,32 @@ return {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep (cwd)" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    {
+      "<leader>en",
+      function()
+        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Neovim Files",
+    },
+    {
+      "<leader>fn",
+      function()
+        require("telescope.builtin").find_files({
+          search_dirs = {
+            vim.fn.expand("~/Obsidian/engineer/"),
+            vim.fn.expand("~/Documents/Notes/"),
+          },
+        })
+      end,
+      desc = "Find Notes and Documents",
+    },
+    -- {
+    --   "<leader>fp",
+    --   function()
+    --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+    --   end,
+    --   desc = "Find Plugin Files",
+    -- },
   },
   opts = {
     pickers = {
