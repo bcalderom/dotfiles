8a8399cfb69f5358d91a38134243a33ffdc352ec
bcalderom  <65696302+bcalderom@users.noreply.github.com>
2026-01-12 16:27:53 -0300

    feat(nvim/telescope): add file browser extension and keymap
    - Add telescope-file-browser dependency
    - Add <leader>fb to open file browser in current file’s directory
    - Configure file_browser extension mappings (insert/normal)
    - Load file_browser extension during Telescope setup
    - Rename live_grep key description to be cwd-agnostic

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,13 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
+
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep (cwd)" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
     {
       "<leader>en",
@@ -27,23 +29,79 @@ return {
       end,
       desc = "Find Notes and Documents",
     },
-    -- {
-    --   "<leader>fp",
-    --   function()
-    --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-    --   end,
-    --   desc = "Find Plugin Files",
-    -- },
+    {
+      "<leader>fb",
+      function()
+        require("telescope").extensions.file_browser.file_browser({
+          path = vim.fn.expand("%:p:h"),
+          cwd = vim.fn.expand("%:p:h"),
+          select_buffer = true,
+          hidden = true,
+          respect_gitignore = false,
+          grouped = true,
+          initial_mode = "insert",
+        })
+      end,
+      desc = "File Browser (here)",
+    },
   },
+
   opts = {
     pickers = {
       find_files = {
         find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
       },
     },
+
+    extensions = {
+      file_browser = {
+        mappings = {
+          i = {
+            ["<C-n>"] = "create",          -- new file / dir
+            ["<C-r>"] = "rename",          -- rename
+            ["<C-d>"] = "remove",          -- delete
+            ["<C-m>"] = "move",            -- move
+            ["<C-c>"] = "copy",            -- copy
+            ["<C-h>"] = "goto_parent_dir",
+            ["<C-l>"] = "goto_home_dir",
+            ["<C-w>"] = "goto_cwd",
+          },
+          n = {
+            ["n"] = "create",
+            ["r"] = "rename",
+            ["d"] = "remove",
+            ["m"] = "move",
+            ["c"] = "copy",
+            ["h"] = "goto_parent_dir",
+            ["~"] = "goto_home_dir",
+            ["w"] = "goto_cwd",
+          },
+        },
+      },
+    },
   },
+
   config = function(_, opts)
-    require("telescope").setup(opts)
-    pcall(require("telescope").load_extension, "fzf")
+    local telescope = require("telescope")
+
+    -- resolve string action names to real functions
+    local ok, fb_actions = pcall(function()
+      return require("telescope").extensions.file_browser.actions
+    end)
+
+    if ok then
+      local maps = opts.extensions.file_browser.mappings
+      for _, mode in pairs({ "i", "n" }) do
+        for k, v in pairs(maps[mode]) do
+          if type(v) == "string" and fb_actions[v] then
+            maps[mode][k] = fb_actions[v]
+          end
+        end
+      end
+    end
+
+    telescope.setup(opts)
+    pcall(telescope.load_extension, "fzf")
+    pcall(telescope.load_extension, "file_browser")
   end,
 }
